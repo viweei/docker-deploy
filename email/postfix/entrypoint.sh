a4bea831fa57e75f4209ddafb92261626dbab116
viweei <viweei@126.com>
2025-02-06 10:16:14 +0800

    add environment to postfix

--- email/postfix/entrypoint.sh
+++ email/postfix/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-DB_HOST=$(dig +short mysql)
-DB_PORT=3306
-DB_USER=root
-DB_PASSWORD=123456
-DB_NAME=mailserver
 
 DOVECOT_HOST=$(dig +short dovecot)
 DOVECOT_PORT=12345
 
-postconf -e smtpd_sasl_path=inet:$DOVECOT_HOST:$DOVECOT_PORT
+postconf -e smtpd_sasl_path=inet:${DOVECOT_HOST}:${DOVECOT_PORT}
+
+export DB_HOST=$(dig +short mysql)
+export DB_PORT=3306
+export DB_USER=root
+export DB_PASSWORD=123456
+export DB_NAME=mailserver
 
 envsubst < /etc/postfix/sql/alias.cf.template > /etc/postfix/sql/alias.cf
 envsubst < /etc/postfix/sql/domains.cf.template > /etc/postfix/sql/domains.cf
