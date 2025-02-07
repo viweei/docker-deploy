3abd0e80513c4d09a76fa7207553783128bae418
viweei <viweei@126.com>
2025-02-07 09:26:35 +0800

    try config postfix

--- email/postfix/entrypoint.sh
+++ email/postfix/entrypoint.sh
@@ -10,11 +10,11 @@ envsubst < /etc/postfix/sql/alias.cf.template > /etc/postfix/sql/alias.cf
 envsubst < /etc/postfix/sql/domains.cf.template > /etc/postfix/sql/domains.cf
 envsubst < /etc/postfix/sql/mailbox.cf.template > /etc/postfix/sql/mailbox.cf
 
+DOVECOT_HOST=$(dig +short dovecot | head -n 1)
+postconf -e smtpd_sasl_path=inet:${DOVECOT_HOST}:12345
+postconf -e virtual_transport=lmtp:${DOVECOT_HOST}:24
+
 # Start Postfix
 postfix start
-
-export DOVECOT_HOST=$(dig +short dovecot)
-export DOVECOT_PORT=12345
-postconf -e smtpd_sasl_path=inet:${DOVECOT_HOST}:${DOVECOT_PORT}
 
 tail -f /var/log/postfix.log 
